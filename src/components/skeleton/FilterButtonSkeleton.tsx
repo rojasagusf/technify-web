f4d72d2c45b52ebc9205d2b6332f3c78feae210c
Agustin Rojas <rojasagusf01@gmail.com>
2024-01-23 17:12:28 -0300

    Add skeleton of button filter

---
+++ src/components/skeleton/FilterButtonSkeleton.tsx
@@ -0,0 +1,9 @@
+export default function FilterButtonSkeleton() {
+  return (
+    <div className='flex justify-start gap-2 mb-2'>
+      <div className="bg-activeGray w-1/4 min-h-8 rounded-full" />
+      <div className="bg-activeGray w-2/4 min-h-8 rounded-full" />
+      <div className="bg-activeGray w-1/4 min-h-8 rounded-full" />
+    </div>
+  )
+}
